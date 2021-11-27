5514b7346b8c5a6783ea9e6f12bef051bdf8e1e4
praneethsai83 <praneethsai83@gmail.com>
2021-11-27 17:17:28 +0530

    Updated jsonParser.scala

--- src/scala/main/json_parser/jsonParser.scala
+++ src/scala/main/json_parser/jsonParser.scala
@@ -1,5 +1,6 @@
 package scala.main.json_parser
 
 object jsonParser extends SparkSessionWrapper with App {
-  
+  var df = spark.read.json("C:\\Users\\HP\\Documents\\workspace\\json_parser\\resources\\data.json")
+  df.show
 }
